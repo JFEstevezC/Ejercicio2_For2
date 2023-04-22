10f5d7e3c6b86607684d893a2b8a04db102b242a
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-04-22 07:20:30 -0500

    Ejercicios

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,7 +1,7 @@
 # Mostar e imprimir cuantos múltiplos de 7 y cuántos múltiplos de 9 hay entre 1000 y 5000.
 m7=0
 m9=0
-for i in range(1000,5000):
+for i in range(1000,5001):
     a = i % 7
     b = i % 9
     if a == 0:
